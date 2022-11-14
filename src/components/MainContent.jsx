f7ca1e908c871bce926046b931d6f25b3c218e09
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-14 21:19:17 +0100

    run report is tests fail:: by sergio giraldo @ 20221114T2119CET, gpg signed

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import HelpArea from './HelpArea';
+import HelpArea from "./HelpArea";
 
 function MainContent() {
-  const [helpVisible, setHelpVisible] = useState(false);
+	const [helpVisible, setHelpVisible] = useState(false);
 
-  function toggleHelp() {
-    setHelpVisible((isVisible) => !isVisible);
-  }
+	function toggleHelp() {
+		setHelpVisible((isVisible) => !isVisible);
+	}
 
-  return (
-    <main>
-      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
-      {helpVisible && <HelpArea />}
-    </main>
-  );
+	return (
+		<main>
+			<button onClick={toggleHelp}>
+				{helpVisible ? "Hide" : "Show"} Help
+			</button>
+			{helpVisible && <HelpArea />}
+		</main>
+	);
 }
 
 export default MainContent;
